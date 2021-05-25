d938ec79a7eeab20e4899737942d9f3011f99634
Ekatereana <you@example.com>
2021-05-26 00:52:24 +0300

    refactor resoures files + add quote service mapping

--- app/src/main/java/marketplanner/stocks/Settings.java
+++ app/src/main/java/marketplanner/stocks/Settings.java
@@ -1,32 +1,11 @@
-// vim: set sts=4 sw=4 tw=99 et:
-//
-// Copyright (C) 2019 AlliedModders LLC
-// Copyright (C) 2019 David Anderson
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package marketplanner.stocks;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-
-import net.alliedmods.quotes.AlphaVantage;
-import net.alliedmods.quotes.IQuoteService;
-import net.alliedmods.quotes.IexCloud;
-import net.alliedmods.quotes.Intrinio;
-import net.alliedmods.quotes.WorldTradingData;
+import marketplanner.quotes.*;
 
 import java.io.File;
 import java.io.FileInputStream;
--- app/src/main/java/marketplanner/stocks/SymbolSearchThread.java
+++ app/src/main/java/marketplanner/stocks/SymbolSearchThread.java
@@ -1,30 +1,9 @@
-// vim: set sts=4 sw=4 tw=99 et:
-//
-// Copyright (C) 2019 AlliedModders LLC
-// Copyright (C) 2019 David Anderson
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package marketplanner.stocks;
 
 import android.util.Log;
+import marketplanner.quotes.IQuoteService;
+import marketplanner.quotes.SymbolSuggestion;
 
-import net.alliedmods.quotes.IQuoteService;
-import net.alliedmods.quotes.SymbolSuggestion;
-
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class SymbolSearchThread extends IThread
 {
