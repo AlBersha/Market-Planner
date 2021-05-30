09dbdfd286334bdd41569f5055f2326b5e7372cf
Никита Молодцов <nikita.molodtsov21@gmail.com>
2021-05-31 02:31:15 +0300

    Added fingerprint authentication

--- app/src/main/java/marketplanner/stocks/MainActivity.java
+++ app/src/main/java/marketplanner/stocks/MainActivity.java
@@ -11,13 +11,22 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricManager.Authenticators;
+import androidx.biometric.BiometricPrompt;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import marketplanner.quotes.*;
 
 
+import java.lang.reflect.Executable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -26,6 +35,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
     static final private String TAG = "MainActivity";
@@ -51,6 +61,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         settings_ = StockApplication.getSettings();
+
+//        BiometricManager biometricManager = BiometricManager.from(this);
+//        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+//            case BiometricManager.BIOMETRIC_SUCCESS:
+//                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+//                Log.e("MY_APP_TAG", "No biometric features available on this device.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+//                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+//                // Prompts the user to create credentials that your app accepts.
+//                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+//                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+//                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+//                startActivityForResult(enrollIntent, REQUEST_CODE);
+//                break;
+//        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+        biometricPrompt.authenticate(promptInfo);
     }
 
     @Override
